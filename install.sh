4a326c3598b311d6dfb436f9ba3e2eca69569862
Zach Loeber <zach.loeber@havi.com>
2020-02-02 16:21:26 -0600

    initial commit

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 here=`pwd`
+mkdir -p ${here}/.local/app
+mkdir -p ${here}/.local/bin
+
+rm -rf ${here}/.local/app/ghr-installer
+git clone git@github.com:zloeber/ghr-installer.git ${here}/.local/app/ghr-installer
+
 dotfiles=(
 	".zshrc"
 	".gitconfig"
 	".ssh"
-	".local"
+	".local/bin"
+	".local/app"
 	".oh-my-zsh"
 	".tmux"
 	".tmux.conf.local"
 	".zgen"
 	".zshrc.local"
 	".p10k.zsh")
+
 for dotfile in "${dotfiles[@]}";do
  echo "Linking: ${here}/${dotfile}"
  ln -sf "${here}/${dotfile}" "${HOME}/${dotfile}"
