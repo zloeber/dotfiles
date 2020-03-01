5efada4df9a3766e63a1e3ccfad5a94ed855cd41
Zachary Loeber <zloeber@gmail.com>
2020-02-29 22:19:50 -0600

    update: add sudo script

---
+++ sudo_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Basic items used for many adsf plugins among other things
+apt install \
+  automake autoconf libreadline-dev \
+  libncurses-dev libssl-dev libyaml-dev \
+  libxslt-dev libffi-dev libtool unixodbc-dev \
+  unzip curl
